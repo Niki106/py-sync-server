870d18cd695c9320d354ce663a79f7fa6464d263
Logos106 <logos106@outlook.com>
2024-11-21 16:28:54 +0100

    a

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    nip: str
+    email: str
+    category: str
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-# @app.post("/webhooks")
-# async def handle_webhook(request: Request):
-#     webhook_data = await request.json()  # Parse the JSON payload
-#     # Process the data in webhook_data
-#     print(webhook_data)  # Example: Print the payload for debugging
+@app.post("/webhooks")
+async def handle_webhook(item: Item):
+    print(item)  # Example: Print the payload for debugging
     
-#     return {"message": "Webhook received successfully"}
+    return {"message": "Webhook received successfully"}
