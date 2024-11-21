f38f42de442c34a5a20070a31d690614a9dca699
Logos106 <logos106@outlook.com>
2024-11-21 11:11:15 +0100

    Initial add

---
+++ core/config.py
@@ -0,0 +1,35 @@
+import os
+
+from pydantic_settings import BaseSettings
+
+
+class Config(BaseSettings):
+    ENV: str = "development"
+    DEBUG: bool = True
+    APP_HOST: str = "0.0.0.0"
+    APP_PORT: int = 8000
+
+class TestConfig(Config):
+    WRITER_DB_URL: str = "mysql+aiomysql://fastapi:fastapi@localhost:3306/fastapi_test"
+    READER_DB_URL: str = "mysql+aiomysql://fastapi:fastapi@localhost:3306/fastapi_test"
+
+
+class LocalConfig(Config):
+    ...
+
+
+class ProductionConfig(Config):
+    DEBUG: bool = False
+
+
+def get_config():
+    env = os.getenv("ENV", "local")
+    config_type = {
+        "test": TestConfig(),
+        "local": LocalConfig(),
+        "prod": ProductionConfig(),
+    }
+    return config_type[env]
+
+
+config: Config = get_config()
